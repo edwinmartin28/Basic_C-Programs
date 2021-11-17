817de047d15878d52e1df4373c1b4c5f0899605e
Edwin <edwinkottat123456789@gmail.com>
2021-11-17 22:25:51 +0530

    Updated

---
+++ Perfect_Square_Numbers_Between_a_Range.c
@@ -0,0 +1,21 @@
+//C program to find the perfect square numbers b/w a range.
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i,n,m,temp=0;
+    float d;
+    printf("Enter the first elements :");
+    scanf("%d",&n);
+    printf("Enter the second elements :");
+    scanf("%d",&m);
+    printf("The perfect square numbers between %d and %d : ",n,m);
+    for(i=n;i<m;i++)
+    {
+        d = sqrt(i);
+        temp=d;
+        if(temp==d&&d!=0)
+          printf("%d \t",i);
+    }
+    return 0;
+}
